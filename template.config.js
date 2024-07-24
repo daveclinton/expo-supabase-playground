1ab7de405674a0033034f3ef72f9b937215f7a0b
David Clinton <clinton@192.168.0.100>
2024-07-24 17:14:35 +0300

    Upesi Starter template

--- template.config.js
+++ template.config.js
@@ -2,6 +2,6 @@ module.exports = {
   expo: {
     name: "Upesi Starter",
     slug: "upesi-starter",
-    description: "A comprehensive React Native Expo boilerplate with authentication, state management, and animations"
+    description: "A comprehensive React Native Expo boilerplate"
   }
 };
